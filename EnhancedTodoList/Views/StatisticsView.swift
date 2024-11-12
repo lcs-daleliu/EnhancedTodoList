dda6d1c57bc44d1d3c70317ca4ded1cfbd82c370
Dale Liu <YLiu@lcs.on.ca>
2024-11-12 11:41:45 -0500

    Added tab view.

---
+++ EnhancedTodoList/Views/StatisticsView.swift
@@ -0,0 +1,18 @@
+//
+//  StatisticsView.swift
+//  EnhancedTodoList
+//
+//  Created by Yiren LIU on 2024-11-12.
+//
+
+import SwiftUI
+
+struct StatisticsView: View {
+    var body: some View {
+        Text("Statistics view")
+    }
+}
+
+#Preview {
+    StatisticsView()
+}
---
+++ EnhancedTodoList/Views/TodoListView.swift
@@ -0,0 +1,18 @@
+//
+//  TodoListView.swift
+//  EnhancedTodoList
+//
+//  Created by Yiren LIU on 2024-11-12.
+//
+
+import SwiftUI
+
+struct TodoListView: View {
+    var body: some View {
+        Text("To-do list view")
+    }
+}
+
+#Preview {
+    TodoListView()
+}
